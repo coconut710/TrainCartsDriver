0d8015fe6664e0bdd551e01c8db8c62711d34b03
coconut710 <angryphw@naver.com>
2022-07-24 13:45:46 +0900

    Update TCDriver.java

--- src/main/java/com/sketchtown/bukkit/tcdriver/TCDriver.java
+++ src/main/java/com/sketchtown/bukkit/tcdriver/TCDriver.java
@@ -1,35 +1,63 @@
 package com.sketchtown.bukkit.tcdriver;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 
-public class TCHangRail extends PluginBase {
-    private final List<Driver> driversList = new ArrayList<Driver>();
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import com.bergerkiller.bukkit.common.PluginBase;
+import com.bergerkiller.bukkit.common.Task;
+import com.bergerkiller.bukkit.common.config.FileConfiguration;
+import com.sketchtown.bukkit.tcdriver.commands.TCDriverCommands;
+
+public class TCDriver extends PluginBase {
+    private final Map<Player, Driver> driversList = new HashMap<Player, Driver>();
     private final List<DriveableTrain> trainList = new ArrayList<DriveableTrain>();
+    private Task updateDriveableTrainTask;
 	
-	private TCCoastersCommands commands;
+	private TCDriverCommands commands;
 	private final TCDriverListener listener = new TCDriverListener(this);
-    private final TCDriverInteractionListener interactionListener = new TCDriverInteractionListener(this);
 
     @Override
     public void onLoad() {
         this.getLogger().log(Level.INFO, "Loading TrainCartsDriver");
-        FileConfiguration config = new FileConfiguration(this);
+        FileConfiguration config = new FileConfiguration(this, "");
         config.load();
         config.save();
     }
 
-    @Override
-    public void onEnable() {
-        this.getLogger().log(Level.INFO, "Enabling TrainCartsDriver");
-        this.listener.enable();
-		this.interactionListener.enable();
-    }
+	@Override
+	public int getMinimumLibVersion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
-    @Override
-    public void onDisable() {
-        this.getLogger().log(Level.INFO, "Disabling TrainCartsDriver");
+	@Override
+	public void enable() {
+        this.listener.enable();
+		this.commands = new TCDriverCommands();
+        this.commands.enable(this);
+		
+        this.getLogger().log(Level.INFO, "Enabling TrainCartsDriver");
+	}
+
+	@Override
+	public void disable() {
         this.driversList.clear();
-    }
+		this.listener.disable();
+        Task.stop(this.updateDriveableTrainTask);
+        
+        this.getLogger().log(Level.INFO, "Disabling TrainCartsDriver");
+	}
+
+	@Override
+	public boolean command(CommandSender sender, String command, String[] args) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 }
