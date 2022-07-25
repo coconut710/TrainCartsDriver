5a7d532dc21d7276aa2ab95690d86410b28d267c
coconut710 <angryphw@naver.com>
2022-07-25 22:52:42 +0900

    add vehicle exit/enter event + use isdriveable function

--- src/main/java/com/sketchtown/bukkit/tcdriver/TCDriverListener.java
+++ src/main/java/com/sketchtown/bukkit/tcdriver/TCDriverListener.java
@@ -1,13 +1,17 @@
 package com.sketchtown.bukkit.tcdriver;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.vehicle.VehicleEnterEvent;
+import org.bukkit.event.vehicle.VehicleExitEvent;
 
 import com.bergerkiller.bukkit.tc.controller.MinecartGroup;
+import com.bergerkiller.bukkit.tc.controller.MinecartGroupStore;
 import com.bergerkiller.bukkit.tc.events.GroupLinkEvent;
 import com.bergerkiller.bukkit.tc.events.SignActionEvent;
 import com.bergerkiller.bukkit.tc.signactions.SignAction;
@@ -34,8 +38,8 @@ public class TCDriverListener implements Listener {
 	public void onGroupLink(GroupLinkEvent event) {
 		MinecartGroup g1 = event.getGroup1();
 		MinecartGroup g2 = event.getGroup2();
-		boolean d1 = this.plugin.trainIsDriving(g1);
-		boolean d2 = this.plugin.trainIsDriving(g2);
+		boolean d1 = this.plugin.isDriveable(g1);
+		boolean d2 = this.plugin.isDriveable(g2);
 		if (d1 && !d2) {
         	plugin.addDriveableTrain(g2);
         	plugin.getDriveableTrain(g2).setProperties(plugin.getDriveableTrain(g1));
@@ -48,16 +52,64 @@ public class TCDriverListener implements Listener {
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onPlayerQuitEvent(PlayerQuitEvent event) {
 		Player player = event.getPlayer();
+		plugin.removeDriver(player);
+	}
+	
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onVehicleExitEvent(VehicleExitEvent event) {
+		Entity e = event.getExited();
+		if (!(e instanceof Player)) return;
+		Player player = (Player) e;
+		Driver driver;
+		MinecartGroup group;
+		DriveableTrain driveable;
 		if (plugin.isDriver(player)) {
-			plugin.removeDriver(player);
+			driver = plugin.getDriver(player);
+		} else {
+			return;
 		}
+		group = MinecartGroupStore.get(event.getVehicle());
+		if (group == null) {
+			return;
+		}
+		if (plugin.isDriveable(group)) {
+			driveable = plugin.getDriveable(group);
+		} else {
+			return;
+		}
+		driver.clearMember();
+	}
+	
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onVehicleEnterEvent(VehicleEnterEvent event) {
+		Entity e = event.getEntered();
+		if (!(e instanceof Player)) return;
+		Player player = (Player) e;
+		Driver driver;
+		MinecartGroup group;
+		DriveableTrain driveable;
+		if (plugin.isDriver(player)) {
+			driver = plugin.getDriver(player);
+		} else {
+			return;
+		}
+		group = MinecartGroupStore.get(event.getVehicle());
+		if (group == null) {
+			return;
+		}
+		if (plugin.isDriveable(group)) {
+			driveable = plugin.getDriveable(group);
+		} else {
+			return;
+		}
+		driver.updateMember(driveable);
 	}
 	
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onSignActionEvent(SignActionEvent event) {
 		MinecartGroup group = event.getGroup();
 		DriveableTrain driveable;
-		if (!this.plugin.trainIsDriving(group)) {
+		if (!this.plugin.isDriveable(group)) {
 			return;
 		} else {
 			driveable = plugin.getDriveableTrain(group);
